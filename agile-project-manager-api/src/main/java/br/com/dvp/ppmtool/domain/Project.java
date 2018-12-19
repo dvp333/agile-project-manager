a56554a4f4542026d046c696490356e11fd506b0
Diego Villela Prado <diego.vprado@ans.gov.br>
2018-12-19 20:10:41 -0200

    Add ProjectRepository class

--- agile-project-manager-api/src/main/java/br/com/dvp/ppmtool/domain/Project.java
+++ agile-project-manager-api/src/main/java/br/com/dvp/ppmtool/domain/Project.java
@@ -1,5 +1,111 @@
 package br.com.dvp.ppmtool.domain;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+
+@Entity
 public class Project {
 
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	private String identifier;
+	
+	private String description;
+	
+	private Date startDate;
+	
+	private Date endDate;
+	
+	private Date createdAt;
+	
+	private Date updatedAt;
+	
+	public Project() {
+		super();
+	}
+
+	@PrePersist
+	protected void onCreate () {
+		createdAt = new Date();
+	}
+	
+	@PreUpdate
+	protected void onUpdate () {
+		updatedAt = new Date();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getIdentifier() {
+		return identifier;
+	}
+
+	public void setIdentifier(String identifier) {
+		this.identifier = identifier;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
 }
